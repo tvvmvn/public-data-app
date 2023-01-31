fb99b25ac033bdcf126bcc28fc8bf1d6dbbfc53a
tvvmvn <tvvmvn@gmail.com>
2023-01-31 13:42:35 +0900

    1/31

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React, {useState, useContext, useEffect, useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Map, MapMarker} from 'react-kakao-maps-sdk';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from "recharts";
 
 function fetchData(searchYearCd) {
   const url = `https://apis.data.go.kr/B552061/frequentzoneBicycle/getRestFrequentzoneBicycle?serviceKey=${process.env.REACT_APP_SERVICE_KEY}&searchYearCd=${searchYearCd}&siDo=11&guGun=110&type=json&numOfRows=10&pageNo=1`;
@@ -23,17 +32,16 @@ export default App;
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState(null);
+  const [items, setItems] = useState(null);
   const [searchYearCd, setSearchYearCd] = useState(2015);
 
-  console.log(data);
-
   useEffect(() => {  
-    // setIsLoaded(false);
+    setIsLoaded(false);
 
     fetchData(searchYearCd)
     .then(data => {
-      setData(data);
+      console.log(data);
+      setItems(data.items.item);
     })
     .catch(error => {
       setError(error)
@@ -49,67 +57,61 @@ function App() {
   }
   return (
     <div style={{margin: "1rem"}}>
-      <h1>Application</h1>
+      <h1>App &#128690;</h1>
 
+      <h2>Choose Year</h2>
       <div className="">
         {/* Event */}
-        <h2>Year</h2>
         <p>{searchYearCd}</p>
         <button onClick={() => setSearchYearCd(searchYearCd - 1)}>Prev</button>
         <button onClick={() => setSearchYearCd(searchYearCd + 1)}>Next</button>
       </div>
 
-      <div className="">
-        <h2>Result</h2>
-        <ul>
-          {data.items.item.map((item, index) => (
-            <li key={index}>{item.spot_nm}</li>
-          ))}
-        </ul>
+      <h2>Table</h2>
+      <Table items={items} />
+
+      <h2>Chart</h2>
+      <div style={{height: "200px"}}>
+        <Rechart items={items} />
       </div>
 
-      <div>
-        <h2>Map</h2>
-        <KakaoMap />
-      </div>
+      <h2>Map</h2>
+      <KakaoMap items={items} />
 
-      <div>
-        <h2>Chart</h2>
-        <div style={{height: "200px"}}>
-         <Rechart />
-        </div>
-      </div>
     </div>  
   )
 }
 
-function KakaoMap(){
-  const positions = [
-    {
-      title: "카카오",
-      latlng: { lat: 33.450705, lng: 126.570677 },
-    },
-    {
-      title: "생태연못",
-      latlng: { lat: 33.450936, lng: 126.569477 },
-    },
-    {
-      title: "텃밭",
-      latlng: { lat: 33.450879, lng: 126.56994 },
-    },
-    {
-      title: "근린공원",
-      latlng: { lat: 33.451393, lng: 126.570738 },
-    },
-  ]
+function Table({items}) {
 
   return (
-    <Map // 지도를 표시할 Container
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
+    <ul>
+      {items.map(item => (
+        <li key={item.spot_nm}>{item.spot_nm}</li>
+      ))}
+    </ul>
+  )
+}
+
+function KakaoMap({items}){
+
+  const center = {
+    lat: items[0].la_crd,
+    lng: items[0].lo_crd
+  }
+
+  const positions = items.map(item => {
+    return {
+      title: item.spot_nm,
+      latlng: {lat: item.la_crd , lng: item.lo_crd}
+    }
+  })
+
+  return (
+    // 지도를 표시할 Container
+    <Map 
+      // 지도의 중심좌표
+      center={center}
       style={{
         // 지도의 크기
         width: "100%",
@@ -117,7 +119,7 @@ function KakaoMap(){
       }}
       level={3} // 지도의 확대 레벨
     >
-      {positions.map((position, index) => (
+      {positions.map(position => (
         <MapMarker
           key={`${position.title}-${position.latlng}`}
           position={position.latlng} // 마커를 표시할 위치
@@ -135,38 +137,41 @@ function KakaoMap(){
   )
 }
 
-function Rechart() {
-  const chartData = [
-    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400,},
-    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210,},
-    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290,},
-    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000,},
-    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181,},
-    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500,},
-    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100,},
-  ];
+function Rechart({items}) {
+
+  console.log(items);
+
+  const data = items.map(item => {
+    return {
+      name: item.spot_nm,
+      발생건수: item.occrrnc_cnt,
+      사망자수: item.dth_dnv_cnt,
+      중상자수: item.se_dnv_cnt
+    }
+  })
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <LineChart
-        width={500}
-        height={300}
-        data={chartData}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
-    </ResponsiveContainer>  
+    <BarChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="발생건수" fill="#0bf" />
+      <Bar dataKey="사망자수" fill="#f00" />
+      <Bar dataKey="중상자수" fill="#fa0" />
+    </BarChart>
+  </ResponsiveContainer>
   )
 }
