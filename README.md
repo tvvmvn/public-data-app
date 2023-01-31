ca4261c1f4cf8bb7f34dcc567236e6d6f0d7f4be
tvvmvn <tvvmvn@gmail.com>
2023-01-31 15:46:47 +0900

    1/31

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # Public Data App
 Group Project
 
-## Public-Data API
-
+## 공공데이터 API
+### 공공데이터 포털 
 [https://www.data.go.kr/index.do](https://www.data.go.kr/index.do)
 
-## Kakao Map
+### 예시에서 활용한 자전거 사고조회 데이터
+https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15056681
 
+
+
+## 카카오 지도 API
+### Kakao Map
 [https://apis.map.kakao.com/](https://apis.map.kakao.com/)
+
+### React-Kakao Map
 [https://react-kakao-maps-sdk.jaeseokim.dev/](https://react-kakao-maps-sdk.jaeseokim.dev/)
 
+
+
 ## Chart library
+### Rechart
 [https://recharts.org/en-US](https://recharts.org/en-US)
 
 ## Deploy with Github Pages
 [https://github.com/gitname/react-gh-pages](https://github.com/gitname/react-gh-pages)
+
+
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useContext, useEffect, useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Map, MapMarker} from 'react-kakao-maps-sdk';
+import {Map, MapMarker, MapInfoWindow} from 'react-kakao-maps-sdk';
 import {
   BarChart,
   Bar,
@@ -14,34 +14,40 @@ import {
 } from "recharts";
 
 function fetchData(searchYearCd) {
-  const url = `https://apis.data.go.kr/B552061/frequentzoneBicycle/getRestFrequentzoneBicycle?serviceKey=${process.env.REACT_APP_SERVICE_KEY}&searchYearCd=${searchYearCd}&siDo=11&guGun=110&type=json&numOfRows=10&pageNo=1`;
 
-  const promise = fetch(url)
-  .then(res => {
-    if (!res.ok) {
-      throw res;
-    }
-    return res.json();
-  })
+  const endPoint = 'https://apis.data.go.kr/B552061/frequentzoneBicycle/getRestFrequentzoneBicycle'
+  const serviceKey = process.env.REACT_APP_SERVICE_KEY;
+  const siDo = 11;
+  const goGun = 440;
+  const type = 'json';
+  const numOfRows = 10
+  const pageNo = 1
+
+  const promise = fetch(`${endPoint}?serviceKey=${serviceKey}&searchYearCd=${searchYearCd}&siDo=${siDo}&guGun=${goGun}&type=${type}&numOfRows=${numOfRows}&pageNo=${pageNo}`)
+    .then(res => {
+      if (!res.ok) {
+        throw res;
+      }
+      return res.json();
+    })
 
   return promise;
 }
 
-export default App;
-
-function App() {
+export default function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState(null);
+  const [data, setData] = useState(null);
   const [searchYearCd, setSearchYearCd] = useState(2015);
+
+  console.log(data);
 
   useEffect(() => {  
     setIsLoaded(false);
 
     fetchData(searchYearCd)
     .then(data => {
-      console.log(data);
-      setItems(data.items.item);
+      setData(data);
     })
     .catch(error => {
       setError(error)
@@ -52,126 +58,95 @@ function App() {
   if (error) {
     return <p>failed to fetch</p>
   }
+
   if (!isLoaded) {
     return <p>fetching data...</p>
   }
+
   return (
     <div style={{margin: "1rem"}}>
-      <h1>App &#128690;</h1>
+      <h1>{searchYearCd}년 서울특별시 마포구 자전거 사고조회 &#128561;</h1>
 
-      <h2>Choose Year</h2>
+      <h2>조회하실 연도를 선택하십시오</h2>
       <div className="">
-        {/* Event */}
-        <p>{searchYearCd}</p>
-        <button onClick={() => setSearchYearCd(searchYearCd - 1)}>Prev</button>
-        <button onClick={() => setSearchYearCd(searchYearCd + 1)}>Next</button>
+        <button onClick={() => setSearchYearCd(searchYearCd - 1)}>&#10094; 이전년도</button>
+        <button onClick={() => setSearchYearCd(searchYearCd + 1)}>다음년도 &#10095;</button>
       </div>
 
-      <h2>Table</h2>
-      <Table items={items} />
+      {data.totalCount > 0 ? (
+        <>
+          <h2>요약</h2>
+          <p>총 {data.totalCount}건의 사고가 발생했습니다</p>
 
-      <h2>Chart</h2>
-      <div style={{height: "200px"}}>
-        <Rechart items={items} />
-      </div>
+          <h2>Chart</h2>
+          <Rechart accidents={data.items.item} />
 
-      <h2>Map</h2>
-      <KakaoMap items={items} />
-
+          <h2>지도</h2>
+          <p>지도를 확대 또는 축소할 수 있습니다</p>
+          <KakaoMap accidents={data.items.item} />
+        </>
+      ) : (
+        <p>해당 년도 자료가 없습니다</p>
+      )}
     </div>  
   )
 }
 
-function Table({items}) {
+function Rechart({accidents}) {
 
-  return (
-    <ul>
-      {items.map(item => (
-        <li key={item.spot_nm}>{item.spot_nm}</li>
-      ))}
-    </ul>
-  )
-}
-
-function KakaoMap({items}){
-
-  const center = {
-    lat: items[0].la_crd,
-    lng: items[0].lo_crd
-  }
-
-  const positions = items.map(item => {
+  const chartData = accidents.map(accident => {
     return {
-      title: item.spot_nm,
-      latlng: {lat: item.la_crd , lng: item.lo_crd}
+      name: accident.spot_nm.split(' ')[2],
+      발생건수: accident.occrrnc_cnt,
+      중상자수: accident.se_dnv_cnt,
+      사망자수: accident.dth_dnv_cnt
     }
   })
 
   return (
-    // 지도를 표시할 Container
-    <Map 
-      // 지도의 중심좌표
-      center={center}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "450px",
-      }}
-      level={3} // 지도의 확대 레벨
+    <div style={{ height: "300px" }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={chartData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="발생건수" fill="#0af" />
+          <Bar dataKey="중상자수" fill="#fa0" />
+          <Bar dataKey="사망자수" fill="#f00" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+function KakaoMap({ accidents }) {
+
+  const mapInfoWindows = accidents.map(accident => (
+    <MapInfoWindow
+      key={accident.spot_nm}
+      position={{ lat: accident.la_crd, lng: accident.lo_crd }}
+      removable={true}
     >
-      {positions.map(position => (
-        <MapMarker
-          key={`${position.title}-${position.latlng}`}
-          position={position.latlng} // 마커를 표시할 위치
-          image={{
-            src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png", // 마커이미지의 주소입니다
-            size: {
-              width: 24,
-              height: 35
-            }, // 마커이미지의 크기입니다
-          }}
-          title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        />
-      ))}
+      <div style={{ padding: "5px", color: "#000" }}>
+        {accident.spot_nm.split(' ')[2]}
+      </div>
+    </MapInfoWindow>
+  ))
+
+  return (
+    <Map
+      center={{ lat: accidents[0].la_crd, lng: accidents[0].lo_crd }}
+      style={{ width: "100%", height: "450px" }}
+      level={5}
+    >
+      {mapInfoWindows}
     </Map>
-  )
-}
-
-function Rechart({items}) {
-
-  console.log(items);
-
-  const data = items.map(item => {
-    return {
-      name: item.spot_nm,
-      발생건수: item.occrrnc_cnt,
-      사망자수: item.dth_dnv_cnt,
-      중상자수: item.se_dnv_cnt
-    }
-  })
-
-  return (
-    <ResponsiveContainer width="100%" height="100%">
-    <BarChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="발생건수" fill="#0bf" />
-      <Bar dataKey="사망자수" fill="#f00" />
-      <Bar dataKey="중상자수" fill="#fa0" />
-    </BarChart>
-  </ResponsiveContainer>
   )
 }
